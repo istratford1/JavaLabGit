6702cacf3794d764bf3e5609f079255edd307620
istratford <istratford1@googlemail.com>
2015-07-21 09:48:53 +0100

    First commit

---
+++ JavaLab_Day2/src/javalab/day2/Day2_Lottery.java
@@ -0,0 +1,63 @@
+package javalab.day2;
+
+import java.util.*;
+
+public class Day2_Lottery {
+
+	
+	public static void main(String[] args){
+		
+		List<Integer> arrLotteryNums = new ArrayList<Integer>(); // type safe array of integers
+		
+		/// fill the array with lotto numbers
+		for(int i=1; i <= 6; i++){
+			int newNum = randomValue(1,50);
+			
+			while(arrLotteryNums.contains(newNum)){ ///make sure that it isn't in the list...
+			  newNum = randomValue(1,50); // it was in the list, so generate a new number
+					
+			}
+			
+			arrLotteryNums.add(newNum);
+			
+		}
+				
+		/// sort the lottery numbers into order...
+		Collections.sort(arrLotteryNums);
+		
+
+		// now get the unique bonus ball
+	
+		int bonusBall = randomValue(1,50);
+		while(arrLotteryNums.contains(bonusBall)){ ///make sure that it isn't in the list...
+		  bonusBall = randomValue(1,50); // it was in the lottery array  list, so change for a new ball
+		}
+		
+		// form the string of numbers to show in the output console
+		String lotteryString = "";
+		
+		for(int n=0; n < arrLotteryNums.size(); n++){
+		  lotteryString = lotteryString +  arrLotteryNums.get(n).toString() + " " ;
+		}
+		
+		// print the final string to the console
+		System.out.println("Lottery numbers: " + lotteryString + " Bonus ball " + String.valueOf(bonusBall) );
+		
+		
+		
+	}
+	
+	/// simple random number generator that gives a random value between two ranges
+	// INPUT: minValue , maxValue
+	
+	static int randomValue(int minValue, int maxValue)
+	{
+	   int range = (maxValue - minValue) + 1; // gives the total range of values that we need minus the minimum value
+	   
+	   return (int)(Math.random() * range) + minValue;
+	}
+	
+	
+	
+	
+}
---
+++ JavaLab_Day2/src/javalab/day2/Day2_StringReverse.java
@@ -0,0 +1,69 @@
+package javalab.day2;
+import java.util.*;
+
+
+public class Day2_StringReverse {
+
+	
+	public static void main(String[] args){
+		
+		Scanner scn = new Scanner(System.in);
+		String userWord = new String(myGetInput("Please enter a word",scn,2));
+		scn.close();
+		
+		int wordLength = userWord.length();
+		String revString = "";
+	
+		
+		for(int i=wordLength -1; i > -1; i--){ // loop from end to start of word, remembering that 0 is the starting index for a string..
+			
+		   String rLetter = Character.toString(userWord.charAt(i));  // last letters decremented by i
+		  	 
+		   revString = revString +  rLetter;
+		   
+	
+		}
+		///
+		
+	
+		System.out.println("Reversed word: " + revString );
+			
+		
+		
+		
+		
+	}
+	
+
+	
+	
+	// MY GET INPUT
+	// gets the user input from keyboard
+	/// uses the java scanner object and console output to prompt the user for input
+	/// INPUT: userMsg - text to display to user
+	//  INPUT: s - scanner object
+	//  INPUT: minLength - the minumum number of chars expected
+	static String myGetInput(String userMsg, Scanner s, int minLength){
+		
+	    /// set initial values
+	    String returnStr = "";
+	    int strLength = 0;
+	    
+	    while (strLength < minLength){
+	    	System.out.println(userMsg + " minimum " + minLength + " letters");
+	    	returnStr = (String) s.next(); // read scanner input into string
+	    	strLength = returnStr.length(); // work out the length
+	    	
+	    	if (strLength >= minLength){
+	    	  break; 
+	    
+	    	} else{
+	    		System.out.println("Try again - must be at least " + minLength + " letters");
+	    	
+	    	}
+	    
+		  }
+	    return returnStr;
+		
+	} 
+}
